bc8c40bec397cde86e4ae20a64c3e9ee996b6f49
Noah Frazier-Logue <nfrazier-logue@research.baycrest.org>
2022-03-23 16:26:01 -0400

    removed sphinx dependency on plspy core deps to save GH Action compute time

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.52.g1900856',
+    VERSION: '0+untagged.53.g1437bad.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
