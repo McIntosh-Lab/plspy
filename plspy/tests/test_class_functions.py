c0ff12723954371c982b412b0d225b3540587416
Noah Frazier-Logue <nfrazier-logue@research.baycrest.org>
2022-03-25 10:54:43 -0400

    added unit test to check CircleCI integration before writing the rest of the tests (probably after small refactor

--- plspy/tests/test_class_functions.py
+++ plspy/tests/test_class_functions.py
@@ -1,6 +1,22 @@
 # TODO: build out unit tests and end-to-end tests
 
+import numpy as np
+import pytest
 
-class TestClass:
-    pass
-    # def test
+import plspy
+
+rand_s = np.random.RandomState(950613)
+
+
+# basic placeholder test for now
+def test_mean_centre():
+    X = rand_s.rand(50,100)
+    cond_order = np.array([[5,5]]*5)
+    res = plspy.class_functions._mean_centre(X, cond_order, mctype=0, return_means=False)
+    assert res.shape == (10, 100)
+    res = plspy.class_functions._mean_centre(X, cond_order, mctype=1, return_means=False)
+    assert res.shape == (10, 100)
+    res = plspy.class_functions._mean_centre(X, cond_order, mctype=2, return_means=False)
+    assert res.shape == (50, 100)
+    
+     
